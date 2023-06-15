a8f765f3ae4d400d3efffbd8bdeb84a6be7e330e
Fabricio Magnus <fabricio.magnus92@outlook.com>
2023-06-15 20:05:04 -0300

    fix: table

--- src/components/exportModal/index.jsx
+++ src/components/exportModal/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Button,
   Modal,
@@ -12,13 +12,31 @@ import {
 } from "@chakra-ui/react";
 import handleExportPDF from "../../functions/exportPDF";
 import { ProgressoPorEtapas } from "../react-circle-progress";
+import { HubConnectionBuilder } from "@microsoft/signalr";
 
 const ModalComponent = ({ isOpen, setIsOpen }) => {
   const handleClose = () => {
     setIsOpen(false);
   };
 
-  const etapaAtual = 12;
+  const [etapaAtual, setEtapaAtual] = useState(15);
+
+  // useEffect(() => {
+  //   const connection = new HubConnectionBuilder()
+  //     .withUrl("https://localhost:5001/etapa")
+  //     .build();
+
+  //   connection.on("ReceiveEtapaAtual", (novaEtapa) => {
+  //     setEtapaAtual(novaEtapa);
+  //   });
+
+  //   connection.start();
+
+  //   return () => {
+  //     connection.stop();
+  //   };
+  // }, []);
+
   const totalEtapas = 17;
 
   return (
